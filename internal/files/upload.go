b17c4c33aa659d4502884d41b1bc22fe6dbddda0
kelche <kelcheone@gmail.com>
2024-08-24 00:28:18 +0300

    feat: get product images by product id from the DO spaces also updated ACL permisions to public

--- internal/files/upload.go
+++ internal/files/upload.go
@@ -2,19 +2,25 @@ package files
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/joho/godotenv"
 )
 
 type Files struct {
 	s3client      *s3.Client
 	presignClient *s3.PresignClient
+	bucketName    string
 }
 
 func NewFileClient() (*Files, error) {
+	godotenv.Load()
 	cfg, err := Config()
 	if err != nil {
 		return nil, err
@@ -25,6 +31,7 @@ func NewFileClient() (*Files, error) {
 	return &Files{
 		s3client:      s3client,
 		presignClient: presignClient,
+		bucketName:    os.Getenv("DO_BUCKET"),
 	}, nil
 }
 
@@ -33,6 +40,8 @@ func (f *Files) GetPresignedURL(keyName string) (string, error) {
 		&s3.PutObjectInput{
 			Bucket: aws.String(os.Getenv("DO_BUCKET")),
 			Key:    aws.String(keyName),
+			// 'x-amz-acl': 'public-read' -- This header is required for public read ACL for Digital ocean
+			ACL: types.ObjectCannedACLPublicRead,
 		},
 		s3.WithPresignExpires(time.Minute*3),
 	)
@@ -41,3 +50,26 @@ func (f *Files) GetPresignedURL(keyName string) (string, error) {
 	}
 	return presignedURL.URL, nil
 }
+
+func (f *Files) GetProductImages(ctx context.Context, productId string) ([]string, error) {
+	st := time.Now()
+	path := fmt.Sprintf("products/%s", productId)
+	params := &s3.ListObjectsV2Input{
+		Bucket: &f.bucketName,
+		Prefix: &path,
+	}
+	res, err := f.s3client.ListObjectsV2(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(time.Since(st))
+
+	var images []string
+	for _, object := range res.Contents {
+		images = append(images, aws.ToString(object.Key))
+		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+	}
+	return images, nil
+}
+
+// in this style you get the product images by the product id then you can use the url to get the images. it is the same as storing the images in the database but this time you store the images in the cloud storage. Later I'll add a cache layer to cache the images for a certain period of time. This is to reduce the number of requests made to the cloud storage when getting the urls of the images.
