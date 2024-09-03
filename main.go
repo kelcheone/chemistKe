af4299c604c6e200ee036ca6e37034216d65830d
kelche <kelcheone@gmail.com>
2024-09-03 11:15:24 +0300

    feat: register the cms service on the server

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 
+	cmsservice "github.com/kelcheone/chemistke/cmd/cms-service"
 	orderservice "github.com/kelcheone/chemistke/cmd/order-service"
 	productservice "github.com/kelcheone/chemistke/cmd/product-service"
 	userservice "github.com/kelcheone/chemistke/cmd/user-service"
 	"github.com/kelcheone/chemistke/internal/database"
 	"github.com/kelcheone/chemistke/internal/files"
+	cms_proto "github.com/kelcheone/chemistke/pkg/grpc/cms"
 	order_proto "github.com/kelcheone/chemistke/pkg/grpc/order"
 	product_proto "github.com/kelcheone/chemistke/pkg/grpc/product"
 	user_proto "github.com/kelcheone/chemistke/pkg/grpc/user"
@@ -53,12 +55,14 @@ func main() {
 	newUservice := userservice.NewService(db)
 	newProductService := productservice.NewProductService(files, db)
 	newOrderService := orderservice.NewOrderService(db)
+	newCmsService := cmsservice.NewCmsService(db)
 
 	grpcServer := grpc.NewServer()
 
 	user_proto.RegisterUserServiceServer(grpcServer, newUservice)
 	product_proto.RegisterProductServiceServer(grpcServer, newProductService)
 	order_proto.RegisterOrderServiceServer(grpcServer, newOrderService)
+	cms_proto.RegisterCmsServiceServer(grpcServer, newCmsService)
 	lis, err := net.Listen("tcp", ":8090")
 	if err != nil {
 		log.Fatalf("Could not start the listener: %v\n", err)
