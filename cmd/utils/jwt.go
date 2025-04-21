b4d56e6770785cf3181be4ced16a91080d53140d
Kevin Kelche <kelcheone@gmail.com>
2025-04-21 13:22:16 +0300

    feat: add new fields to jwt claims.
    - Extract a jwt claims from cookie

--- cmd/utils/jwt.go
+++ cmd/utils/jwt.go
@@ -13,20 +13,26 @@ import (
 
 var secretKey []byte
 
-func int() {
+func init() {
 	godotenv.Load()
 	secretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+	if len(secretKey) == 0 {
+		panic("JWT_SECRET_KEY environment variable is not set")
+	}
 }
 
 type jwtCustomClaims struct {
 	Id     string `json:"id"`
-	Name   string `json:"name"`
+	Name   string `json:"name" example:"John Doe" binding:"required"`
+	Email  string `json:"email" example:"john.doe@example.com" binding:"required,email"`
+	Phone  string `json:"phone" example:"+1234567890" binding:"required,phone"`
 	Admin  bool   `json:"admin"`
 	Author bool   `json:"author"`
 	jwt.RegisteredClaims
 }
 
-func CreateToken(id string, name string, role string) (string, error) {
+func CreateToken(id string, email string, name string, phone string, role string) (string, error) {
 	var admin bool
 	var author bool
 
@@ -42,6 +48,8 @@ func CreateToken(id string, name string, role string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id":     id,
 		"name":   name,
+		"email":  email,
+		"phone":  phone,
 		"admin":  admin,
 		"author": author,
 		"exp":    time.Now().Add(time.Hour * 168).Unix(),
@@ -63,21 +71,51 @@ func ExtractClaims(tokenString string) (*jwtCustomClaims, error) {
 		},
 	)
 	if err != nil {
-		return nil, fmt.Errorf("Could not parse the token.")
+		return nil, fmt.Errorf("could not parse the token")
 	}
 
 	if claims, ok := token.Claims.(*jwtCustomClaims); ok && token.Valid {
 		return claims, nil
 	} else {
-		return nil, fmt.Errorf("Invalid JWT token")
+		return nil, fmt.Errorf("invalid JWT token")
 	}
 }
 
 func ExtractClaimsFromRequest(c echo.Context) *jwtCustomClaims {
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(*jwtCustomClaims)
-
+	user, ok := c.Get("user").(*jwt.Token)
+	if !ok {
+		return nil
+	}
+	claims, ok := user.Claims.(*jwtCustomClaims)
+	if !ok {
+		return nil
+	}
 	return claims
+}
+
+// extract claims from Cookie
+func ExtractClaimsFromCookie(c echo.Context) *jwtCustomClaims {
+	cookie, err := c.Cookie("token")
+	if err != nil {
+		return nil
+	}
+	tokenString := cookie.Value
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&jwtCustomClaims{},
+		func(t *jwt.Token) (interface{}, error) {
+			return secretKey, nil
+		},
+	)
+	if err != nil {
+		return nil
+	}
+
+	if claims, ok := token.Claims.(*jwtCustomClaims); ok && token.Valid {
+		return claims
+	} else {
+		return nil
+	}
 }
 
 func VerifyToken(tokenString string) error {
